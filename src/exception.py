a7c0325b4a35c9be97cb46ceb12dfc2cd575aa21
mukulpm <mundlemba2024@email.iimcal.ac.in>
2024-06-23 02:12:17 +0530

    logging and exp

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
@@ -21,14 +21,14 @@ class CustomException(Exception):
         return self.error_message
     
     
-if __name__=="__main__":
+# if __name__=="__main__":
         
-    try:
-        a=1/0
+#     try:
+#         a=1/0
             
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
     
         
     
